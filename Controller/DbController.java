34e558a8d4b55422b0e4ce8d4acd921f84f007c4
Spikerman <spikechenhao@163.com>
2016-03-24 15:14:17 +0800

    initial commit

---
+++ Controller/DbController.java
@@ -0,0 +1,32 @@
+package Controller;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ * Created by chenhao on 3/24/16.
+ */
+public class DbController {
+    public static final String url = "jdbc:mysql://115.159.100.165/Data";
+    public static final String name = "com.mysql.jdbc.Driver";
+    public static final String user = "Spike";
+    public static final String password = "spike950628";
+    public Connection connection = null;
+
+    public DbController() {
+        try {
+            Class.forName(name);
+            connection = DriverManager.getConnection(url, user, password);
+
+            //connection testing
+            System.out.println("Connect Success");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String args[]){
+
+        DbController dbController = new DbController();
+    }
+}
