6a0503a8de145ab8fd84abb87b334d73434c5c8e
Spikerrman <spikechenhao@163.com>
2017-10-01 10:48:51 -0400

    Update RemoteDbController.java

--- Controller/RemoteDbController.java
+++ Controller/RemoteDbController.java
@@ -8,10 +8,10 @@ import java.sql.PreparedStatement;
  * Created by chenhao on 4/16/16.
  */
 public class RemoteDbController {
-    public static final String url = "jdbc:mysql://115.159.100.165/Data";
+    public static final String url = "";
     public static final String name = "com.mysql.jdbc.Driver";
-    public static final String user = "GroupTie";
-    public static final String password = "grouptie123456";
+    public static final String user = "";
+    public static final String password = "";
     public static final String insertAppGroupSql = "insert into Data.AppGroup (groupId,appId) values (?,?)";
     public static final String insertCandidateClusterSql = "insert into Data.CandidateCluster (clusterId,appId) values (?,?)";
     public Connection connection = null;
